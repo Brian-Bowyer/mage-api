341968c84d5d3da0c96d62176031845e48d49f39
Brian-Bowyer <bobsnake64@gmail.com>
2022-12-03 18:35:30 -0600

    debug info on traceback

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,1 +1,4 @@
+import os
+
 MAX_YANTRA_BONUS = 5
+DEBUG = bool(int(os.getenv("DEBUG", 0)))
--- app/utils/errors.py
+++ app/utils/errors.py
@@ -1,6 +1,10 @@
+import traceback
+
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+
+from app.utils.constants import DEBUG
 
 
 async def handle_exception(
@@ -14,6 +18,14 @@ async def handle_exception(
         message=message,
         extra=kwargs.get("extra", {k: kwargs[k] for k in kwargs}),
     )
+    if DEBUG:
+        payload["debug"] = {
+            "raw_exception": repr(exc),
+            "traceback": "".join(traceback.format_tb(exc.__traceback__)),
+            "request_headers": str(request.headers),
+        }
+        if kwargs.get("debug") is not None and isinstance(kwargs["debug"], dict):
+            payload["debug"] = {**payload["debug"], **kwargs["debug"]}
     return JSONResponse(status_code=status_code, content=payload)
 
 
